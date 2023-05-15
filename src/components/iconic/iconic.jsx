7a6d1afaf4e3d9c6bd0bfc15624452141d16f1a4
Ryan <ryanwanjiedistortion@gmail.com>
2023-05-15 16:31:21 +0300

    nike

---
+++ src/components/iconic/iconic.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './iconic.css'
+
+
+function Iconic() {
+
+
+
+  return (
+
+
+    <>
+
+
+    
+    
+    
+    
+    
+    
+    
+    </>
+
+
+  )
+}
+
+export default Iconic
