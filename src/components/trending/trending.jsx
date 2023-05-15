9c8f7a24708171f654610999000f977095debe15
Ryan <ryanwanjiedistortion@gmail.com>
2023-05-15 14:39:04 +0300

    nike

---
+++ src/components/trending/trending.jsx
@@ -0,0 +1,72 @@
+import React from 'react'
+
+
+
+
+function Trending() {
+
+
+
+  return (
+
+    <>
+
+
+    <section className="trending">
+
+        <h2>Trending</h2>
+
+
+        <div className="image-conts">
+
+            <div className="img">
+
+                <div className='img-1'>
+
+
+                 <img src ='https://unsplash.com/photos/gTTtXwqmKPQ' alt ='' className='nike-img-1'/>
+
+                </div>
+
+
+                <div className='img-2'> 
+                
+                    <img src ='' alt ='' className='nike-img-2'/>
+                
+                </div>
+
+
+
+
+
+            </div>
+
+
+
+
+        </div>
+
+
+
+
+
+    </section>
+
+
+
+
+
+
+
+
+
+
+
+
+    </>
+
+
+  )
+}
+
+export default Trending
