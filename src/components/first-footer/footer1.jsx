ecc3ef01a3ac48847981cbf39468f9e653154e91
Ryan <ryanwanjiedistortion@gmail.com>
2023-05-22 15:39:36 +0300

    nike

--- src/components/first-footer/footer1.jsx
+++ src/components/first-footer/footer1.jsx
@@ -12,6 +12,114 @@ function Footer1() {
     <>
 
 
+        <section className="footer1">
+
+            <div className="middle-footer-1">
+
+
+                <div className="featured">
+
+                    <h5>Featured</h5>
+
+                    <div className="inner-featured">
+
+                        <h6>Air Force 1</h6>
+                        <h6>Jordan 1</h6>
+                        <h6>Metcon</h6>
+                        <h6>Air Max 270</h6>
+
+                    </div>
+
+
+                
+
+                </div>
+
+
+
+                <div className="featured">
+
+                    <h5>Shoes</h5>
+
+                    <div className="inner-featured">
+
+                        <h6>All Shoes</h6>
+                        <h6>Jordan Shoes</h6>
+                        <h6>Running Shoes</h6>
+                        <h6>Basketball Shoes</h6>
+
+                    </div>
+
+                </div>
+
+
+                <div className="featured">
+
+                    <h5>Featured</h5>
+
+                    <div className="inner-featured">
+
+                        <h6>Air Force 1</h6>
+                        <h6>Jordan 1</h6>
+                        <h6>Metcon</h6>
+                        <h6>Air Max 270</h6>
+
+                    </div>
+
+
+                
+
+                </div>
+
+                <div className="featured">
+
+                    <h5>Featured</h5>
+
+                    <div className="inner-featured">
+
+                        <h6>Air Force 1</h6>
+                        <h6>Jordan 1</h6>
+                        <h6>Metcon</h6>
+                        <h6>Air Max 270</h6>
+
+                    </div>
+
+
+                
+
+                </div>
+
+
+
+
+
+
+
+
+
+
+
+
+            </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        </section>
+
+
 
 
 
