ca1149eaccbf30ebe2133acb6d5519e408b72ac6
Ryan <ryanwanjiedistortion@gmail.com>
2023-05-22 13:19:18 +0300

    nike

--- src/components/membership/member.jsx
+++ src/components/membership/member.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './member.css'
-
+import {SiNike} from 'react-icons/si'
 
 
 function Member() {
@@ -12,14 +12,40 @@ function Member() {
 
     <>
 
-    
+
+      <section className="member">
+
+        <h2>Nike Membership</h2>
+
+
+        <div className="membership">
+
+          <div className="membership__item">
+
+            <p className="text-member">BECOME A MEMBER </p>
+
+            <p className="community">Sign up for free. Join the community.</p>
+
+            <button className="btn">Join Us</button>
+            <button className="btn">Sign In</button>
+
+
+          </div>
+
+
+          <div className="nike-logo">
+
+            <SiNike className="nike-logo__icon" />
+
+          
+          </div>
 
 
 
-    
-    
-    
-    
+
+        </div>
+
+      </section>
     
     </>
 
