f9b7403a6581f746da1421da024fb7f47982d730
Ryan <ryanwanjiedistortion@gmail.com>
2023-05-23 12:23:33 +0300

    nike

--- src/components/mainfooter/mainf.jsx
+++ src/components/mainfooter/mainf.jsx
@@ -10,6 +10,63 @@ function Mainf() {
 
     <>
 
+        <footer className="main-footer">
+
+            <section className="inner-footer">
+
+
+                <div className="footer-1">
+
+                  <h5>Gift Cards</h5>
+                  <h5>Promotions</h5>
+                  <h5>Promotions</h5>
+                  <h5>Find a Store</h5>
+                  <h5>Become A Member</h5>
+                  <h5>Nike Journal</h5>
+
+
+
+
+                </div>
+
+                <div className="footer-2">
+
+
+
+                </div>
+
+                <div className="footer-3">
+
+
+
+
+
+
+                </div>
+
+
+                <div className="links">
+
+
+
+                    
+                </div>
+
+
+
+
+
+            </section>
+
+
+
+
+
+
+
+
+        </footer>
+
 
     
     
